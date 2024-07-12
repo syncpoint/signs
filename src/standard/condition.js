100310aefc141c533d9ada680c1ae4c55d884920
dehmer <horst.dehmer@syncpoint.io>
2024-07-12 06:57:56 +0200

    added optional outline to operational condition.

--- src/standard/condition.js
+++ src/standard/condition.js
@@ -1,16 +1,23 @@
 import * as BBox from '../bbox.js'
 
 export const condition = options => {
-  return box => {
+  if (!options.condition) return box => [box, []]
+  else return box => {
     const [width] = BBox.extent(box)
     const y = box[3] + 5
-    const instruction = {
+
+    const styles =
+      options.outline
+        ? ['style:condition', 'style:outline']
+        : ['style:condition']
+
+    const instructions = styles.map(style => ({
       type: 'path',
       d: `M${box[0]},${y} l${width},0 0,25 -${width},0 z`,
-      ...options['style:condition']
-    }
+      ...options[style]
+    }))
 
-    const bbox = BBox.merge(box, BBox.of(instruction))
-    return [bbox, [instruction]]
+    const bbox = BBox.merge(box, BBox.of(instructions))
+    return [bbox, instructions]
   }
 }
